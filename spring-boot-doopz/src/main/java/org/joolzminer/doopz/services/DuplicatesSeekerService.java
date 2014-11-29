d8364f22d79b4ee61fe1c3d772fed74f0c8ece0b
Sergio F. Gonzalez <sergio.f.gonzalez@gmail.com>
2014-11-29 18:31:07 +0100

    Adds logging.

--- spring-boot-doopz/src/main/java/org/joolzminer/doopz/services/DuplicatesSeekerService.java
+++ spring-boot-doopz/src/main/java/org/joolzminer/doopz/services/DuplicatesSeekerService.java
@@ -8,12 +8,16 @@ import java.util.Set;
 import java.util.TreeMap;
 
 import org.joolzminer.doopz.domain.FileInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
 @Service
 public class DuplicatesSeekerService {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(DuplicatesSeekerService.class);
 	
 	@Autowired
 	private FileRetrieverService fileRetrieverService;
@@ -23,7 +27,10 @@ public class DuplicatesSeekerService {
 	
 	public Map<Long, Set<FileInfo>> getDuplicatesByLength(String fromDirectory) {
 		validateFromDir(fromDirectory);
+		LOGGER.debug("loading files from `{}`", fromDirectory);
 		List<File> files = fileRetrieverService.getFiles(fromDirectory);
+		
+		LOGGER.debug("building map of files with equal length");
 		Map<Long, Set<FileInfo>> fileMapByLen = fileMapBuilderService.buildFileMapByFileLength(files);
 		Map<Long, Set<FileInfo>> dupsByLen = new TreeMap<>();
 		for (Entry<Long, Set<FileInfo>> entry : fileMapByLen.entrySet()) {
@@ -31,13 +38,17 @@ public class DuplicatesSeekerService {
 				dupsByLen.put(entry.getKey(), entry.getValue());
 			}
 		}
+				
 		return dupsByLen;
 	}
 	
 	
 	public Map<String, Set<FileInfo>> getDuplicatesByName(String fromDirectory) {
 		validateFromDir(fromDirectory);
+		LOGGER.debug("loading files from `{}`", fromDirectory);
 		List<File> files = fileRetrieverService.getFiles(fromDirectory);
+		
+		LOGGER.debug("building map of files with same name");
 		Map<String, Set<FileInfo>> fileMapByName = fileMapBuilderService.buildFileMapByFileName(files);
 		Map<String, Set<FileInfo>> dupsByName = new TreeMap<>();
 		for (Entry<String, Set<FileInfo>> entry : fileMapByName.entrySet()) {
@@ -45,10 +56,13 @@ public class DuplicatesSeekerService {
 				dupsByName.put(entry.getKey(), entry.getValue());
 			}
 		}
+
 		return dupsByName;
 	}
 	
 	private void validateFromDir(String fromDirectory) {
+		LOGGER.debug("checking validity of `{}`", fromDirectory);
+		
 		Assert.hasText(fromDirectory, "the argument `fromDirectory` must not be null or empty");
 		
 		File dir = new File(fromDirectory);
--- spring-boot-doopz/src/main/java/org/joolzminer/doopz/services/FileRetrieverService.java
+++ spring-boot-doopz/src/main/java/org/joolzminer/doopz/services/FileRetrieverService.java
@@ -4,14 +4,23 @@ import java.io.File;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
 @Service
 public class FileRetrieverService {
 	
+	private static final Logger LOGGER = LoggerFactory.getLogger(FileRetrieverService.class);
+	
 	public List<File> getFiles(String baseDirectory) {
 		Assert.hasText(baseDirectory, "base directory must not be null or empty");
-		return (List<File>) FileUtils.listFiles(new File(baseDirectory), null, true);
+		
+		LOGGER.debug("collecting files under directory `{}`", baseDirectory);
+		List<File> files = (List<File>) FileUtils.listFiles(new File(baseDirectory), null, true);
+		LOGGER.debug("{} file(s) found in `{}`", files.size(), baseDirectory); 
+		
+		return files;
 	}
 }
